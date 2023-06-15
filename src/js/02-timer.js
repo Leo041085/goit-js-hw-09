3615551c2b0d066ca5f5452af4208a054890e0c4
Leo041085 <fullsleons@gmail.com>
2023-06-15 12:50:20 +0300

    comit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
 const refs = {
     startBtn: document.querySelector('button[data-start'),
-    stopBtn: document.querySelector('button[data-stop]'),
+    
     clockface: document.querySelector('')
 }
