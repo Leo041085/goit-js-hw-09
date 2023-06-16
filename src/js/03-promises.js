0aeaec04383052604bec302bb040a8de60fe27a1
Leo <fullsleons@gmail.com>
2023-06-16 21:08:16 +0300

    hw3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
+};
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  
+  const delay = +evt.target.elements.delay.value;
+  const step = +evt.target.elements.step.value;
+  const amount = +evt.target.elements.amount.value;
+
+  for (let i = 1; i <= amount; i += 1){
+    createPromise(i, delay + (i - 1) * step )
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
-}
+};
+
+form.addEventListener('submit', handleSubmit);
+
+
