8565ccfb1aa34ecee6b5772fe82930fb93e0ae32
Leo <fullsleons@gmail.com>
2023-06-16 19:08:11 +0300

    hw2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,59 @@
-const refs = {
-    startBtn: document.querySelector('button[data-start'),
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-    clockface: document.querySelector('')
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  Picker: document.querySelector('#datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+let time;
+let countdown;
+
+function updateTimer() {
+  const timeLeft = countdown - Date.now();
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  if (timeLeft <= 0) {
+    clearInterval(time);
+    Notiflix.Notify.success('Countdown is done!!!');
+    refs.startBtn.disabled = false;
+    return;
+  }
+
+  refs.days.textContent = days < 10 ? `0${days}` : days;
+  refs.hours.textContent = hours < 10 ? `0${hours}` : hours;
+  refs.minutes.textContent = minutes < 10 ? `0${minutes}` : minutes;
+  refs.seconds.textContent = seconds < 10 ? `0${seconds}` : seconds;
 }
-console.log("Helllo");
+
+function startTimer() {
+  countdown = new Date(refs.Picker.value).getTime();
+  if (countdown <= Date.now()) {
+    Notiflix.Notify.warning('Сhoose another date !!!');
+    return;
+  }
+  refs.startBtn.disabled = true;
+  updateTimer();
+  time = setInterval(updateTimer, 1000);
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+};
+
+flatpickr(refs.Picker, options);
+
+refs.startBtn.addEventListener('click', startTimer);
